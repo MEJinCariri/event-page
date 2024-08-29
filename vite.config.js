fbbf879eff10fe74a07c38a4c6e7fe8ffb1ea871
FelipeFerraz4 <felipeferraz2402@gmail.com>
2024-08-29 16:14:18 -0300

    workflows of the deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/event-page/', // Altere isso para o caminho do seu projeto
   plugins: [react()],
 })
